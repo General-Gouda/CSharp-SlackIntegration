b84d9688432f0df271bdddf58e81d7afe4141db1
Matt Marchese <matthew.marchese1@gmail.com>
2016-09-21 09:07:12 -0400

    Initial commit

---
+++ SlackIntegrations/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SlackIntegrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlWithAccessToken = "https://hooks.slack.com/services/{webhook information from Slack API}";
+
+            SlackClient client = new SlackClient(urlWithAccessToken);
+
+            string dateTime = DateTime.Now.ToString();
+
+            SlackColors slackColor = new SlackColors()
+            {
+                Red = "#FF0000",
+                Yellow = "#FFFF00",
+                Green = "#00FF00",
+                LightBlue = "#00FFFF",
+                DarkBlue = "#0000FF",
+                Black = "#000000",
+                White = "#FFFFFF",
+                Gray = "#CCCCCC"
+            };
+
+            client.PostMessage(
+                username: "Boaty McBoaterson",
+                text: dateTime + "\nThis is a test message generated in C#!",
+                color: slackColor.Black,
+                pretext: "Testing!"
+            );
+        }
+    }
+}
---
+++ SlackIntegrations/Slack.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+//A simple C# class to post messages to a Slack channel
+//Note: This class uses the Newtonsoft Json.NET serializer available via NuGet
+namespace SlackIntegrations
+{
+    public class SlackClient
+    {
+        private readonly Uri _uri;
+        private readonly Encoding _encoding = new UTF8Encoding();
+
+        public SlackClient(string urlWithAccessToken)
+        {
+            _uri = new Uri(urlWithAccessToken);
+        }
+
+        //Post a message using simple strings
+        public void PostMessage(string text,
+                                string pretext = null,
+                                string username = null,
+                                string channel = null,
+                                string color = null
+                               )
+        {
+            Payload payload = new Payload()
+            {
+                Channel = channel,
+                Username = username,
+                Text = text,
+                Color = color,
+                PreText = pretext,
+            };
+
+            PostMessage(payload);
+        }
+
+        //Post a message using a Payload object
+        public void PostMessage(Payload payload)
+        {
+            string payloadJson = JsonConvert.SerializeObject(payload);
+
+            using (WebClient client = new WebClient())
+            {
+                NameValueCollection data = new NameValueCollection();
+                data["payload"] = payloadJson;
+
+                var response = client.UploadValues(_uri,"POST",data);
+
+                //The response text is usually "ok"
+                string responseText = _encoding.GetString(response);
+            }
+        }
+    }
+
+    //This class serializes into the Json payload required by Slack Incoming WebHooks
+    public class Payload
+    {
+        [JsonProperty("channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+
+        [JsonProperty("fallback")]
+        public string Fallback { get; set; }
+
+        [JsonProperty("color")]
+        public string Color { get; set; }
+
+        [JsonProperty("pretext")]
+        public string PreText { get; set; }
+    }
+
+    // Class that allows for setting colors that appear within new Slack notifications
+    public class SlackColors
+    {
+        public string Red { get; set; }
+        public string Yellow { get; set; }
+        public string Green { get; set; }
+        public string LightBlue { get; set; }
+        public string DarkBlue { get; set; }
+        public string Black { get; set; }
+        public string White { get; set; }
+        public string Gray { get; set; }
+    }
+}
+
